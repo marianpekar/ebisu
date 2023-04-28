956c483a0fdb5bec090ed2895633a69aa5a18031
Marian Pekár <marian.pekar@gmail.com>
2023-04-28 02:33:12 +0200

    Add texture cache

--- src/texture_loader.cpp
+++ src/texture_loader.cpp
@@ -2,13 +2,21 @@
 #include <SDL.h>
 #include <SDL_image.h>
 
+std::unordered_map<const char*, SDL_Texture*> TextureLoader::texture_cache;
+
 SDL_Texture* TextureLoader::LoadTexture(const char* filepath, SDL_Renderer* renderer, int& width, int& height)
 {
+	if (texture_cache.contains(filepath))
+		return texture_cache[filepath];
+
 	SDL_Surface* surface = IMG_Load(filepath);
 	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
 	width = surface->w;
 	height = surface->h;
 	SDL_FreeSurface(surface);
+
+	texture_cache[filepath] = texture;
+
 	return texture;
 }
 
--- src/texture_loader.h
+++ src/texture_loader.h
@@ -1,8 +1,12 @@
 #pragma once
 
+#include <unordered_map>
+
 class TextureLoader 
 {
+private:
+	static std::unordered_map<const char*, struct SDL_Texture*> texture_cache;
 public:
-	static struct SDL_Texture* LoadTexture(const char* filepath, struct SDL_Renderer* renderer, int& width, int& height);
-	static struct SDL_Texture* LoadTexture(const char* filepath, struct SDL_Renderer* renderer);
+	static SDL_Texture* LoadTexture(const char* filepath, struct SDL_Renderer* renderer, int& width, int& height);
+	static SDL_Texture* LoadTexture(const char* filepath, SDL_Renderer* renderer);
 };
